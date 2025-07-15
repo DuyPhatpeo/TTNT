c5dec66099bd4175384f1141cb02db7af0de21f8
Tran Duy Phat <phattranduy00@gmail.com>
2025-07-15 15:16:17 +0700

    buoi 2

---
+++ leodoi.py
@@ -0,0 +1,63 @@
+def readmtk(filename):
+    with open(filename, "r") as f:
+        n = int(f.readline().strip())
+        print(n)
+        adj = []
+        for i in range(n):
+            line = list(map(int, f.readline().split()))
+            print(line)
+            adj.append(line)
+    return n, adj
+
+def readh(filename):
+    with open(filename, "r") as f:
+        n = int(f.readline().strip())
+        h = list(map(int, f.readline().split()))
+        print(h)
+    return h
+
+def HillClimbingSearch(n, adj, h, start, stop):
+    OPEN = [start]
+    CLOSE = []
+    PARENT = [-1] * n
+    while len(OPEN) > 0:
+        cur = OPEN.pop(0)
+        print(f"cur = {cur}")
+        CLOSE.append(cur)
+        print(f"CLOSE = {CLOSE}")
+        if cur == stop:
+            print(f"Tìm thấy đường đi từ {start} -> {stop}")
+            path = []
+            i = stop
+            while i != -1:
+                path.append(i)
+                i = PARENT[i]
+            path.reverse()
+            print("Đường đi:", " -> ".join(map(str, path)))
+            return
+        # tìm các đỉnh kề với cur
+        Tn = []
+        for i in range(n):
+            if adj[cur][i] == 1 and i not in OPEN and i not in CLOSE:
+                Tn.append(i)
+                PARENT[i] = cur
+        print(f"Tn = {Tn}")
+        
+        
+        # sắp xếp theo heuristic tăng dần
+        Tn_sort = sorted(Tn, key=lambda X: h[X])
+        print(f"Tn_sort = {Tn_sort}")
+        
+        # thêm vào OPEN
+        OPEN = Tn_sort + OPEN
+        
+        print(f"OPEN = {OPEN}")
+        print(f"PARENT = {PARENT}")
+    # hết vòng while
+    print(f"Khong tìm thấy đường đi từ {start} đến {stop}")
+
+
+if __name__ == "__main__":
+    n, adj = readmtk("leodoi.mtk")
+    h = readh("leodoi.h")
+    HillClimbingSearch(n, adj, h, 0, 8)
